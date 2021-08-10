415499b6d1da21728dac154b3267118bef10b098
Steve-Hsu <wuujdsc@gmail.com>
2021-08-11 06:59:29 +0700

    redis and mysql basic setting

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { getData, createDate } = require('../controllers/controllers')
 
 // Mount controller
-// router.route('/').get(getData).post(createDate);
+router.route('/').post(createDate);
 router.route('/:id').get(getData);
-router.get('/', (req, res) => {
-  res.sendFile(__dirname.slice(0, -6) + 'index.html');
-});
+// router.route('/:id').get(getData);
+// router.get('/', (req, res) => {
+//   res.sendFile(__dirname.slice(0, -6) + 'index.html');
+// });
 
 
 module.exports = router;
